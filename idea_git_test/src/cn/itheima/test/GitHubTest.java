971efbd8c82582007ad3f8449e665c6ce3b7581e
Zhaodq <534607883@qq.com>
2018-12-22 19:03:59 +0800

    GitHubTest中添加caculation方法

--- idea_git_test/src/cn/itheima/test/GitHubTest.java
+++ idea_git_test/src/cn/itheima/test/GitHubTest.java
@@ -4,4 +4,8 @@ public class GitHubTest {
     public static void main(String[] args) {
         System.out.println("测试github");
     }
+    public int caculation(int a, int b){
+        return a+b;
+    }
 }
+
